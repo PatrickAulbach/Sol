c7d35dd7db55495398126e37a25d0618838dae11
PatrickAulbach <Patrick.Aulbach@gmx.net>
2023-04-03 22:44:35 +0200

    refactored Planetmanager

---
+++ Assets/Scripts/PlanetController.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetController : MonoBehaviour
+{
+    [SerializeField] public List<Sprite> sprites;
+
+   
+}
--- Assets/Scripts/PlanetManager.cs
+++ Assets/Scripts/PlanetManager.cs
@@ -4,10 +4,9 @@ using UnityEngine;
 
 public class PlanetManager : MonoBehaviour
 {
-    [SerializeField] int spawnChance;
-    [SerializeField] List<Sprite> sprites;
+    [SerializeField] PlanetController planetController;
     [SerializeField] int childPosition;
-    private const float INITIAL_SPRITE_SCALE_FACTOR = 0.5f;
+    private const float ADDITIONAL_SPRITE_SCALE_FACTOR = 0.7f;
     private readonly Vector2 CANVAS_SIZE = new Vector2(3, 0.5f);
     private readonly Vector3 CANVAS_SPAWN_POINT = new Vector3(0, 1.6f, 0);
     private readonly List<Vector3> SYMBOL_SPAWN_POINTS = new List<Vector3>{
@@ -15,29 +14,27 @@ public class PlanetManager : MonoBehaviour
         new Vector3(0, 0, 0),
         new Vector3(-1.2f, 0, 0)
     };
-    
-    private void Awake()
-    {
-        Debug.Log(transform.name);
+    void Start()
+    {    
         InitializeCanvas();
-        InitializeRessources();
+        InitializeRessources(); 
     }
 
-    private void InitializeRessources()
+     public void InitializeRessources()
     {
-        for (int i = 0; i < sprites.Count; i++)
+        for (int i = 0; i < planetController.sprites.Count; i++)
         {
-            if (Random.Range(0, 101) <= spawnChance)
+            if (Random.Range(0, 101) <= (int) OptionValues.difficulty)
             {
                 GameObject planetSymbolGameObject = new GameObject();
                 planetSymbolGameObject.transform.localScale = planetSymbolGameObject.transform.localScale / transform.localScale.x;
                 SpriteRenderer spriteRenderer = planetSymbolGameObject.AddComponent<SpriteRenderer>();
 
-                spriteRenderer.sprite = sprites[i];
+                spriteRenderer.sprite = planetController.sprites[i];
                 spriteRenderer.transform.position = SYMBOL_SPAWN_POINTS[i] / transform.localScale.x;
                 if (spriteRenderer.transform.localScale.x >= 1)
                 {
-                    spriteRenderer.transform.localScale *= (INITIAL_SPRITE_SCALE_FACTOR / transform.localScale.x);
+                    spriteRenderer.transform.localScale *= ADDITIONAL_SPRITE_SCALE_FACTOR;
                 }
 
                 Transform canvasTransform = transform.GetChild(childPosition).gameObject.GetComponent<Canvas>().transform;
@@ -46,14 +43,14 @@ public class PlanetManager : MonoBehaviour
         }
     }
 
-    private void InitializeCanvas()
+    public void InitializeCanvas()
     {
         GameObject canvasGameObject = new GameObject();
         var canvas = canvasGameObject.AddComponent<Canvas>();
         canvas.transform.position = CANVAS_SPAWN_POINT;
         canvas.GetComponent<RectTransform>().sizeDelta = CANVAS_SIZE / transform.localScale.x;
 
-        canvas.transform.SetParent(this.transform, false);
+        canvas.transform.SetParent(transform, false);
 
     }
 }
